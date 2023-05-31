6b8c9beafd0e6b0c73a84f8d2e11ab781504fa64
Çağlar Çankaya <89020166+caglarcankaya-jotform@users.noreply.github.com>
2023-05-31 23:10:24 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,19 @@
-print("sasasasasasasasasasasasasasasasasasasasasasasasasasasadssadasdasdasasasasasasassasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasa")
+def   greet(   name):
+    print(   f"Hello, {name}!"   )
+
+def add_numbers(  num1, num2  ):
+    result = num1   +   num2
+    return result
+
+def main():
+    name = "Alice"
+    greeting = greet(name)
+    print(greeting)
+
+    num1 = 10
+    num2 = 5
+    result = add_numbers(num1, num2)
+    print(result)
+
+if __name__ == "__main__":
+    main()
