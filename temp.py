daa9de2289d2f19833a948c551ac9f5c1d2cd812
Çağlar Çankaya <89020166+caglarcankaya-jotform@users.noreply.github.com>
2023-06-01 09:27:52 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,22 +1,4 @@
-def greet(name):
-    print(f"Hello, {name}!")
-
-
-def add_numbers(num1, num2):
-    result = num1 + num2
-    return result
-
-
-def main():
-    name = "Alice"
-    greeting = greet(name)
-    print(greeting)
-
-    num1 = 10
-    num2 = 5
-    result = add_numbers(num1, num2)
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
+def aa(a , b,    c, d):
+    a = [1,2,3,4,1,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,41,2,3,4]
+    
+print("aaa")
